915e1037251a03c093f9a759b3090475c0cc0385
Carlos Sotelo <carlos.sotelo7@gmail.com>
2019-07-24 16:56:38 +0100

    fix: removed wrong breakline character

--- scripts/addSharedToManifest.js
+++ scripts/addSharedToManifest.js
@@ -1,13 +1,11 @@
 console.log("Running hook to apply shared user id to Manifest...");
 module.exports = function (context) {
-    debugger;
-    var xml2js = require('xml2js');
     var fs = context.requireCordovaModule('fs'),
         path = context.requireCordovaModule('path');
-​
+
     var platformRoot = path.join(context.opts.projectRoot, 'platforms/android/app/src/main');
     var manifestFile = path.join(platformRoot, 'AndroidManifest.xml');
-​
+
     var APP_GROUP_ANDROID = "";
     if (process.argv.join("|").indexOf("APP_GROUP_ANDROID=") > -1) {
         APP_GROUP_ANDROID = process.argv.join("|").match(/APP_GROUP_ANDROID=(.*?)(\||$)/)[1]
@@ -15,13 +13,13 @@ module.exports = function (context) {
         var config = fs.readFileSync("config.xml").toString()
         APP_GROUP_ANDROID = getPreferenceValue(config, "APP_GROUP_ANDROID")
     }
-​
+
     if (fs.existsSync(manifestFile)) {
         fs.readFile(manifestFile, 'utf8', function (err, data) {
             if (err) {
                 throw new Error('Unable to find AndroidManifest.xml: ' + err);
             }
-​
+
             if (data.indexOf('android:sharedUserId') == -1) {
                 var result = data.replace(/<manifest/g, '<manifest android:sharedUserId="' + APP_GROUP_ANDROID +' android:sharedUserLabel="@string/cordova_appgroups_shared_label" ');
                 fs.writeFile(manifestFile, result, 'utf8', function (err) {
